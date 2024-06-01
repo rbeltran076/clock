de9992063dbe70dbd1641b3b77efccadb3387097
Ronald Beltrán <rbeltran076@gmail.com>
2024-06-01 17:20:34 +0200

    changed to PyQt. Stable

--- scripts/register.py
+++ scripts/register.py
@@ -2,30 +2,25 @@ import json
 import csv
 import os
 
-# From saves.json to clockdata.csv
-def register_from_json():
+def register_to_csv():
     # Check if the JSON file exists
-    if not os.path.exists('data/saves.json'):
+    if not os.path.exists('clock/data/saves.json'):
         print("No 'saves.json' file found.")
         return
 
     # Load data from JSON file
-    with open('data/saves.json', 'r') as file:
+    with open('clock/data/saves.json', 'r') as file:
         data = json.load(file)
 
     # Check if the CSV file exists, if not create it and write the header
-    if not os.path.exists('data/clockdata.csv'):
-        with open('data/clockdata.csv', 'w', newline='') as file:
+    if not os.path.exists('clock/data/workHours.csv'):
+        with open('clock/data/workHours.csv', 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['Date', 'Title', 'Hourly Rate', 'Time', 'Pay'])
+            writer.writerow(['Date', 'Hourly Rate', 'Time', 'Pay'])
 
     # Write data to CSV file
-    with open('data/clockdata.csv', 'a', newline='') as file:
+    with open('clock/data/workHours.csv', 'a', newline='') as file:
         writer = csv.writer(file)
-        for date, titles in data.items():
-            for title, entries in titles.items():
-                for entry in entries:
-                    writer.writerow([date, title, entry['hourly_rate'], entry['time'], entry['pay']])
-
-# Call the function
-register_from_json()
+        for date, entries in data.items():
+            for entry in entries:
+                writer.writerow([date, entry['hourly_rate'], entry['time'], entry['pay']])
